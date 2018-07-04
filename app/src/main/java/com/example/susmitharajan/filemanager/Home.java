8075f662714d2be0b0c61c8a4a7acbe08410571f
susmitharajan <susmithasundararajan6@gmail.com>
2018-07-04 21:28:21 +0530

    Navigation Bar Added

---
+++ app/src/main/java/com/example/susmitharajan/filemanager/Home.java
@@ -0,0 +1,27 @@
+package com.example.susmitharajan.filemanager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by susmitharajan on 04/07/18.
+ */
+
+public class Home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("File Manager");
+    }
+}
---
+++ app/src/main/java/com/example/susmitharajan/filemanager/Internal_Storage.java
@@ -0,0 +1,27 @@
+package com.example.susmitharajan.filemanager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by susmitharajan on 04/07/18.
+ */
+
+public class Internal_Storage extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.internal_storage, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Internal Storage");
+    }
+}
